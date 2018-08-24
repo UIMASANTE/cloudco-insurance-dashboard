3a5304308608ba2027d6da7b590744e7274654cc
Xavier <UIMASANTE@users.noreply.github.com>
2018-08-24 09:45:02 +0200

    fix ??

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,1 +1,32 @@
+import React from 'react';
+import Card from 'components/Card';
+import CardHeader from 'components/CardHeader';
+import { palette } from 'styles/muiTheme';
+import MessageList from './MessageList';
+import classes from './ChatBox.scss';
 
+const styles = {
+  header: {
+    borderBottom: `1px solid ${palette.accent1Color}`,
+    marginBottom: '1rem'
+  }
+};
+
+const ChatBox = ({ time, log, user }) => (
+  <Card className={classes.container}>
+    <CardHeader
+      title="LOG CONVERSATION"
+      subtitle={`Début: ${time}`}
+      style={styles.header}
+    />
+    <MessageList user={user} log={log} />
+  </Card>
+);
+
+ChatBox.propTypes = {
+  user: React.PropTypes.string.isRequired,
+  log: React.PropTypes.array.isRequired,
+  time: React.PropTypes.string.isRequired//,
+};
+
+export default ChatBox;
